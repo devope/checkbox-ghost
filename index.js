5b8ac05f4b9820095a567ed947257ef2cb88fc0c
Kirill Kirill <mail@devope.ru>
2020-05-13 19:55:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-function initGhostCheckbox(){
-	$('.checkbox-ghost').click(function(){
-		let hiddenPrev = $(this).parents('.checkbox').find('input[type=text]');
-		if($(hiddenPrev).val() == 0 || !$(hiddenPrev).val()){
-			$(hiddenPrev).val(1);
+// wrapper (string): jquery selector
+
+function checkboxGhost(wrapper)
+{
+	let ghosts = document.getElementsByClassName('checkbox-ghost');
+	for (var i = 0; i < ghosts.length; i++) {
+		if(ghosts[i].value == ""){
+			ghosts[i].value = 0;
+		}
+	}
+
+	$('.checkbox-entity').click(function()
+	{
+		let ghost = $(this).parents(wrapper).find('.checkbox-ghost');
+
+		if($(this).is(':checked')){
+			$(ghost).val(1);
 		} else {
-			$(hiddenPrev).val(0);
+			$(ghost).val(0);
 		}
 	});
 }
